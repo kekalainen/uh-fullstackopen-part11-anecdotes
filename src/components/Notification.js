769add83ffb79d59bbfb02789d0abfb72dbbe280
Kekalainen <git@kekalainen.me>
2022-05-17 00:06:16 +0300

    6.3: Formatting

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -2,13 +2,10 @@ const Notification = () => {
   const style = {
     border: 'solid',
     padding: 10,
-    borderWidth: 1
-  }
-  return (
-    <div style={style}>
-      render here notification...
-    </div>
-  )
-}
+    borderWidth: 1,
+  };
 
-export default Notification
+  return <div style={style}>render here notification...</div>;
+};
+
+export default Notification;
