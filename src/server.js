9034b9a10b8ddc9f5bfc8608e7d23277e311b30b
Kekalainen <git@kekalainen.me>
2023-12-22 11:04:43 +0200

    Implement production frontend server

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('build'));
+
+app.get('/healthz', (_req, res) => {
+  res.status(204).send();
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT} 🚀`);
+});
